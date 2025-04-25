5eedc2177ea87225dd8b4ff19d441cb3fbbba691
Erkin <erkint@gmail.com>
2025-04-26 00:04:25 +0600

    ☑️ Refactor classes to implement updated interface

--- src/service/PaymentMethod/BankAccountPaymentMethod.java
+++ src/service/PaymentMethod/BankAccountPaymentMethod.java
@@ -5,24 +5,11 @@ import model.Client;
 public class BankAccountPaymentMethod implements PaymentMethod {
     @Override
     public boolean transfer(Client from, Client to, double amount) {
-//        Validate
-        if (amount <= 0) {
-            System.err.println("Ошибка: сумма перевода должна быть положительной.");
-            return false;
-        }
-
-        if (from.getBalance() < amount) {
-            System.err.println("Ошибка: недостаточно средств у отправителя.");
-            return false;
-        }
-
-//      Validate BankAccount
-        if (to.getBankAccount() == null || to.getBankAccount().isEmpty()) {
+        if (!canTransferTo(to)) {
             System.err.println("Ошибка: у получателя отсутствует банковский счет.");
             return false;
         }
 
-//      Transfer successfully
         from.sendMoney(amount);
         to.receiveMoney(amount);
 
@@ -30,6 +17,11 @@ public class BankAccountPaymentMethod implements PaymentMethod {
                 " от " + from.getFirstName() + " " + from.getLastName() +
                 " к " + to.getFirstName() + " " + to.getLastName());
         return true;
+    }
+
+    @Override
+    public boolean canTransferTo(Client receiver) {
+        return receiver.getBankAccount() != null && !receiver.getBankAccount().isEmpty();
     }
 
     @Override
--- src/service/PaymentMethod/PhonePaymentMethod.java
+++ src/service/PaymentMethod/PhonePaymentMethod.java
@@ -5,31 +5,22 @@ import model.Client;
 public class PhonePaymentMethod implements PaymentMethod {
     @Override
     public boolean transfer(Client from, Client to, double amount) {
-//        Validate
-        if (amount <= 0) {
-            System.err.println("Ошибка: сумма перевода должна быть положительной.");
-            return false;
+        if (canTransferTo(to)) {
+            System.err.println("Ошибка: у получателя отсутствует номер телефона");
         }
 
-        if (from.getBalance() < amount) {
-            System.err.println("Ошибка: недостаточно средств у отправителя.");
-            return false;
-        }
-
-//        Validate Telephone
-        if (to.getPhoneNumber() == null || to.getPhoneNumber().isEmpty()) {
-            System.err.println("Ошибка: у получателя отсутствует номер телефона.");
-            return false;
-        }
-
-//        Transfer successfully
         from.sendMoney(amount);
         to.receiveMoney(amount);
 
-        System.out.println("Перевод по номеру телефона выполнен: " + amount +
+        System.out.println();System.out.println("Перевод по номеру телефона выполнен: " + amount +
                 " от " + from.getFirstName() + " " + from.getLastName() +
                 " к " + to.getFirstName() + " " + to.getLastName());
         return true;
+    }
+
+    @Override
+    public boolean canTransferTo(Client receiver) {
+        return receiver.getPhoneNumber() != null && !receiver.getPhoneNumber().isEmpty();
     }
 
     @Override
